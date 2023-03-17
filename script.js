cffcdbc24e0bc03c29a2f61dfe1d45a77715553a
RexRegum8 <jdlvanonymous@gmail.com>
2023-03-17 13:54:52 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout(cambiam, 2000)
+
+function cambiam() {
+    document.getElementById("cargando").innerHTML="Enviado!"}
+
+setTimeout(function (){
+    alert("Recibiste un abrazo, espero que te mejores 🍕 " )
+},3000)
